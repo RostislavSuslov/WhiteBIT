16328d9612c6ccb75f743a394a83c9a7dfb99e81
RostSuslov <suslovros@gmail.com>
2023-02-02 19:58:10 +0200

    redesighn first screen

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,8 @@
 const socialList = document.querySelector('.person-info__social');
+const personPhoto = document.querySelector('.person-photo img');
 let socialListItem = document.querySelectorAll('.person-info__social-item');
  
+ 
 socialListItem.length == 4 ? socialList.style.width = 100+"%" : null;
+socialListItem.length == 5 ? personPhoto.classList.add('small-photo') : null;
+ 
