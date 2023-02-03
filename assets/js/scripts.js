d431ee6a6fbac4c9c97bfc5f7f10b5169548baeb
RostSuslov <suslovros@gmail.com>
2023-02-03 20:23:08 +0200

    test safary alert

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,16 @@ const socialList = document.querySelector('.person-info__social');
 const personPhoto = document.querySelector('.person-photo img');
 let socialListItem = document.querySelectorAll('.person-info__social-item');
  
- 
 socialListItem.length == 4 ? socialList.style.width = 100+"%" : null;
 socialListItem.length == 5 ? personPhoto.classList.add('small-photo') : null;
+
+
+var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+if(isSafari){
+    alert('user agent safary')
+} else{
+    alert('not safary')
+}
+	
  
