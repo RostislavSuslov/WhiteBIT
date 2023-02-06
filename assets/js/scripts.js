68ae62edf2feddfb78d14371065fa3bbfb715271
RostSuslov <suslovros@gmail.com>
2023-02-06 19:19:45 +0200

    add class

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,16 @@
 const socialList = document.querySelector('.person-info__social');
 const personPhoto = document.querySelector('.person-photo img');
 let socialListItem = document.querySelectorAll('.person-info__social-item');
- 
+const page = document.querySelector('.person-page');
+
 socialListItem.length == 4 ? socialList.style.width = 100+"%" : null;
 socialListItem.length == 5 ? personPhoto.classList.add('small-photo') : null;
 
 
-// var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
-// if(isSafari){
-//     alert('user agent safary')
-// } else{
-//     alert('not safary')
-// }
+if(isSafari){
+    page.classList.add('safary')
+} 
 	
  
