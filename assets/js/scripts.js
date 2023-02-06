e56d992125302b2299469a53d72f8d40d1d3a494
RostSuslov <suslovros@gmail.com>
2023-02-06 17:29:16 +0200

    fix first screen

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,12 +6,12 @@ socialListItem.length == 4 ? socialList.style.width = 100+"%" : null;
 socialListItem.length == 5 ? personPhoto.classList.add('small-photo') : null;
 
 
-var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+// var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
-if(isSafari){
-    alert('user agent safary')
-} else{
-    alert('not safary')
-}
+// if(isSafari){
+//     alert('user agent safary')
+// } else{
+//     alert('not safary')
+// }
 	
  
